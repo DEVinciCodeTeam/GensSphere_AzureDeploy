82718383f1e33e4971169ea2a0f39a223f7a396c
ErickOrtiz0298 <a13003218@alumnos.uady.mx>
2023-06-12 21:34:43 -0600

    se corrijieron errores del pom y del app

--- src/main/java/com/gensphere/ProjectoGensphere/ProjectoGensphereApplication.java
+++ src/main/java/com/gensphere/ProjectoGensphere/ProjectoGensphereApplication.java
@@ -1,13 +1,28 @@
 package com.gensphere.ProjectoGensphere;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import com.gensphere.ProjectoGensphere.storage.StorageProperties;
+import com.gensphere.ProjectoGensphere.storage.StorageService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class ProjectoGensphereApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectoGensphereApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+//			storageService.deleteAll();
+			storageService.init();
+		};
+	}
 }
