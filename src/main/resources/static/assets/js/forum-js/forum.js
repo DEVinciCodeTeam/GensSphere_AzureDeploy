50b04f0ed85585d33a114e2f5e66dd8d7d08e8e8
Mario Rosas <mario_netta12@hotmail.com>
2023-06-18 00:42:22 -0600

    Tring with all forums

--- src/main/resources/static/assets/js/forum-js/forum.js
+++ src/main/resources/static/assets/js/forum-js/forum.js
@@ -1,6 +1,5 @@
 validateCurrentUser();
 
-
 const forumName = document.location.pathname.split("/")[3].split(".")[0];
 
 if (forumName === "forum") {forumFunctionality("forum1Posts")} else {forumFunctionality(forumName+"Posts")}
